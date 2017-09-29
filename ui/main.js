9f56536fe1dbe959360cb5e008a58d595b7d167e
shivabhaskar <31137603+shivabhaskar@users.noreply.github.com>
2017-09-29 11:09:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var user = document.getElementById('user');
+var pass = document.getElementById('pass');
+var loge = document.getElementById('button');
+loge.onclick = function ()
+{
+    if(user== hai && pass==hello)
+    {
+        return ui/about.html;
+    }
+    else
+    {
+        alert ('wrong credintals');
+    }
+};
